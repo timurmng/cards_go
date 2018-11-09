205dfc8d904b31cb6cf1a273a9f7deebf61b831d
Timur Mengazi <timur.mengazi@gmail.com>
2018-11-09 15:11:30 +0200

    better random number generator

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create new type of 'deck' => slice of strings (similar to extends in OOP)
@@ -57,3 +59,12 @@ func newDeckFromFile(filename string) Deck {
 	return Deck(s)
 }
 
+func (d Deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for index := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[index], d[newPosition] = d[newPosition], d[index]
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
+
 }
